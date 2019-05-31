a0723ec9eee13a6460f241e60fca2dec32ee8fcb
Ralph-Gordon Paul <g.paul@appcom-interactive.de>
2019-05-31 15:52:33 +0200

    Add conan scripts for curl

---
+++ Readme.md
@@ -0,0 +1,70 @@
+# Conan curl
+
+This repository contains the conan receipe that is used to build the curl packages at [rgpaul bintray](https://bintray.com/manromen/rgpaul).
+
+For Infos about curl please visit [curl.haxx.se](https://curl.haxx.se/).
+The library is licensed under the [curl License](https://curl.haxx.se/docs/copyright.html).
+This repository is licensed under the [MIT License](LICENSE).
+
+## Android
+
+The environmental `ANDROID_NDK_PATH` must be set to the path of the android ndk.
+
+To create a package for Android you can run the following commands like:
+
+`export ANDROID_NDK_PATH='/opt/android-ndks/android-ndk-r19c'`
+`conan create . curl/7.65.0@rgpaul/stable -s os=Android -s os.api_level=21 -s compiler=clang -s compiler.version=8.0 -s compiler.libcxx=libc++ -s build_type=Release -o android_ndk=r19c -o android_stl_type=c++_static -s arch=x86_64`
+
+### Requirements
+
+* [CMake](https://cmake.org/)
+* [Conan](https://conan.io/)
+* [Android NDK r19c](https://developer.android.com/ndk/downloads/)
+
+## Debian 9 (Stretch)
+
+To create a package for Debian you can run the conan command like this:
+
+`conan create . curl/7.65.0@rgpaul/stable -s os=Linux -s arch=x86_64 -s build_type=Release -o shared=False`
+
+### Requirements
+
+* [CMake](https://cmake.org/)
+* [Conan](https://conan.io/)
+* build-essential, make, curl, git, unzip and zip (`apt-get install build-essential cmake curl git unzip zip`)
+
+## iOS
+
+To create a package for iOS you can run the conan command like this:
+
+`conan create . curl/7.65.0@rgpaul/stable -s os=iOS -s os.version=12.1 -s arch=armv7 -s build_type=Release -o shared=False`
+
+### Requirements
+
+* [CMake](https://cmake.org/)
+* [Conan](https://conan.io/)
+* [Xcode](https://developer.apple.com/xcode/)
+
+## macOS
+
+To create a package for macOS you can run the conan command like this:
+
+`conan create . curl/7.65.0@rgpaul/stable -s os=Macos -s os.version=10.14 -s arch=x86_64 -s build_type=Release -o shared=False`
+
+### Requirements
+
+* [CMake](https://cmake.org/)
+* [Conan](https://conan.io/)
+* [Xcode](https://developer.apple.com/xcode/)
+
+## Windows 10
+
+To create a package for Windows 10 you can run the conan command like this:
+
+`conan create . curl/7.65.0@rgpaul/stable -s os=Windows -s compiler="Visual Studio" -s compiler.runtime=MT -s arch=x86 -s build_type=Release -o shared=False`
+
+### Requirements
+
+* [CMake](https://cmake.org/)
+* [Conan](https://conan.io/)
+* [Visual Studio 2017](https://visualstudio.microsoft.com/de/downloads/)
---
+++ conanfile.py
@@ -0,0 +1,76 @@
+from conans import ConanFile, CMake, tools
+import os
+
+class CurlConan(ConanFile):
+    name = "curl"
+    version = "7.65.0"
+    author = "Ralph-Gordon Paul (gordon@rgpaul.com)"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "android_ndk": "ANY", "android_stl_type":["c++_static", "c++_shared"]}
+    default_options = "shared=False", "android_ndk=None", "android_stl_type=c++_static"
+    description = "command line tool and library for transferring data with URLs"
+    url = "https://github.com/Manromen/conan-curl-scripts"
+    license = "curl"
+    exports_sources = "cmake-modules/*"
+    generators = "cmake_paths"
+
+    # download sources
+    def source(self):
+        url = "https://curl.haxx.se/download/curl-%s.tar.gz" % self.version
+        tools.get(url)
+
+        tools.replace_in_file("%s/curl-%s/CMakeLists.txt" % (self.source_folder, self.version),
+            "include(Utilities)",
+            '''include(${CMAKE_BINARY_DIR}/conan_paths.cmake) 
+            include(Utilities)''')
+
+    # compile using cmake
+    def build(self):
+        cmake = CMake(self)
+        library_folder = "%s/curl-%s" % (self.source_folder, self.version)
+        cmake.verbose = True
+
+        if self.settings.os == "Android":
+            cmake.definitions["CMAKE_SYSTEM_VERSION"] = self.settings.os.api_level
+            cmake.definitions["CMAKE_ANDROID_NDK"] = os.environ["ANDROID_NDK_PATH"]
+            cmake.definitions["CMAKE_ANDROID_NDK_TOOLCHAIN_VERSION"] = self.settings.compiler
+            cmake.definitions["CMAKE_ANDROID_STL_TYPE"] = self.options.android_stl_type
+
+        if self.settings.os == "iOS":
+            ios_toolchain = "cmake-modules/Toolchains/ios.toolchain.cmake"
+            cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = ios_toolchain
+            cmake.definitions["BUILD_TESTING"] = "OFF"
+            cmake.definitions["BUILD_CURL_EXE"] = "OFF"
+            cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = tools.to_apple_arch(self.settings.arch)
+            if self.settings.arch == "x86" or self.settings.arch == "x86_64":
+                cmake.definitions["IOS_PLATFORM"] = "SIMULATOR"
+            else:
+                cmake.definitions["IOS_PLATFORM"] = "OS"
+
+        if self.settings.os == "Macos":
+            cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = tools.to_apple_arch(self.settings.arch)
+
+        cmake.configure(source_folder=library_folder)
+        cmake.build()
+        cmake.install()
+
+    def package(self):
+        self.copy("*", dst="include", src='include')
+        self.copy("*.lib", dst="lib", src='lib', keep_path=False)
+        self.copy("*.dll", dst="bin", src='bin', keep_path=False)
+        self.copy("*.so", dst="lib", src='lib', keep_path=False)
+        self.copy("*.dylib", dst="lib", src='lib', keep_path=False)
+        self.copy("*.a", dst="lib", src='lib', keep_path=False)
+        
+    def requirements(self):
+        self.requires("libressl/2.9.2@%s/%s" % (self.user, self.channel))
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.includedirs = ['include']
+
+    def config_options(self):
+        # remove android specific option for all other platforms
+        if self.settings.os != "Android":
+            del self.options.android_ndk
+            del self.options.android_stl_type
